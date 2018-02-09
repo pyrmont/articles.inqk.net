2cb5a16c048277cae9720fb0d1083dcb0cf87203
Michael Camilleri <mike@inqk.net>
2018-02-09 11:53:17 +0900

    Rename Liquid filter to be a verb.

--- _plugins/colophon_filter.rb
+++ _plugins/colophon_filter.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module ColophonFilter
-    def colophon(input)
+    def add_colophon(input)
       fn_pos = input.index '<div class="footnotes">'
       lp_pos = (fn_pos) ? input.rindex('</p>', fn_pos) : input.rindex('</p>')
       output = (lp_pos) ? input.insert(lp_pos, ' &#10042;') : input
