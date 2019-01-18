c825b1032a5cdd8d5f28a52d36794dcc395bb327
Michael Camilleri <mike@inqk.net>
2019-01-19 03:12:39 +0900

    Fix dangling colophons

--- _plugins/colophon_filter.rb
+++ _plugins/colophon_filter.rb
@@ -3,7 +3,7 @@ module Jekyll
     def add_colophon(input)
       fn_pos = input.index '<section class="footnotes">'
       lp_pos = (fn_pos) ? input.rindex('</p>', fn_pos) : input.rindex('</p>')
-      output = (lp_pos) ? input.insert(lp_pos, ' &#10042;') : input
+      output = (lp_pos) ? input.insert(lp_pos, '&nbsp;&#10042;') : input
     end
   end
 end
